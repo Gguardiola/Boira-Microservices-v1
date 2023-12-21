8e295f3bd4fc368eaa3285b116da6aab225427a7
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-12-21 21:34:27 +0100

    added unignored files

---
+++ dboira/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT
+);
+
+-- Insert some sample data
+INSERT INTO posts (title, content) VALUES
+    ('First Post', 'This is the content of the first post.'),
+    ('Second Post', 'This is the content of the second post.');
