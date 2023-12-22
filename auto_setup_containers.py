36d950dfd1890da28454b7512a5d98edc1d911e1
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-12-22 13:10:39 +0100

    testing

--- auto_setup_containers.py
+++ auto_setup_containers.py
@@ -3,16 +3,16 @@
 import os
 from auto_config import MS_NAME, IMG_TAG, IMG_SERVICES, TARGET_DIR, COMPOSER_FILENAME
 
-print("Loading images...")
-for i in range(len(IMG_SERVICES)):
-    os.system("docker load -i " + TARGET_DIR + "/"+ IMG_SERVICES[i] + ".tar")
-    print("Loaded " + IMG_SERVICES[i] + " image")
-
-print("Done!")
-
+#print("Loading images...")
+#for i in range(len(IMG_SERVICES)):
+#    os.system("docker load -i " + TARGET_DIR + "/"+ IMG_SERVICES[i] + ".tar")
+#    print("Loaded " + IMG_SERVICES[i] + " image")
+#
+#print("Done!")
+#
 opt = input("Do you want to run the containers? (y/n) ")
 if(opt == "y"):
     print("Running containers...")
     #start remote composer
-    os.system("docker-compose -f "+ COMPOSER_FILENAME +" up -d --force-recreate --build")
+    os.system("docker-compose -f "+ COMPOSER_FILENAME +" up -d")
     print("Done!") 
--- dboira/init.sql
+++ dboira/init.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS posts (
 
 -- Insert some sample data
 INSERT INTO posts (title, content) VALUES
-    ('First Post', 'This is the content of the first post.'),
+    ('First Postasdasd', 'This is the content of the first post.'),
     ('Second Post', 'This is the content of the second post.');
